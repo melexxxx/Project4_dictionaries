a5b875e6370253cbfe05c4ee1c0a6d04c86ff905
Daniel Mieleniewski <daniel.mieleniewski@gmail.com>
2023-07-04 15:34:13 +0100

    Project5_Comparisons_Type_Coercion Assignment

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+function typeOf_funciton() { //defying typeOf function 
+    document.getElementById("typeOf").innerHTML = typeof "Black";
+  }
+  
+  function combine() { //defying combine function
+    document.getElementById("combine").innerHTML = "10" + 5;
+  }
+  
+  function operators() {// defying operators check function ==
+    document.getElementById("operators").innerHTML = 55 == 55;
+  }
+  
+  function operators2() {// defying operators check function ===
+    document.getElementById("operators2").innerHTML = "elo" === 5;
+  }
+  
+  function operators3() {// defying operators check function >
+    document.getElementById("operators3").innerHTML = 10 > 4;
+  }
+  
+  function operators4() {// defying operators check function <
+    document.getElementById("operators4").innerHTML = 5 < 3;
+  }
+  
+  function operators5() {// defying operators check function AND
+    document.getElementById("operators5").innerHTML = 2 > 3 && 3 > 1;
+  }
+  
+  function operators6() {// defying operators check function OR
+    document.getElementById("operators6").innerHTML = 10 == 10 || 3 == 2;
+  }
+  
+  function operators7() {// defying operators check function NOT
+    document.getElementById("operators7").innerHTML = 5 != 3;
+  }
+  
